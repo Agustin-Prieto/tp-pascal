26238f62c0b7a5d3b8ce24a7172a5c219c01e11e
Agustin Prieto <agustinprieto.ar@gmail.com>
2018-10-04 23:43:04 -0300

    modulo abm

--- TP3.pas
+++ TP3.pas
@@ -1,11 +1,12 @@
 program tp3(input,output);  {Direccion de los archivos: C:\Tp-pascal\TP3.pas}
-uses crt;
+uses crt, sysutils;
 type
     pago_en_pesos_y_peso_actual = array[1..2,1..2] of Real;
     cantidad_repeticiones = array[1..4,1..50] of Integer;
 
 	gimnasio = record 								{Declaracion de los registros para los archivos}
-		nombre,direccion: String[30];
+		nombre: String[30];
+        direccion: String[30];
 		valor_cuota: Real;
 		valor_nutricionista: Real;
 		valor_personal_trainer: Real;
@@ -44,16 +45,149 @@ type
 		borrado_logico: Boolean;
 	end;
 
-    gim = file of gimnasio;
-    act = file of actividades;
-    d_y_h = file of dias_y_horarios;
-    eje_x_rut = file of ejercicios_x_rutina;
-    cli = file of clientes;
-    rut_x_cli = file of rutinas_x_clientes;
+    gimna = file of gimnasio;
+    acti = file of actividades;
+    dia_y_hor = file of dias_y_horarios;
+    ejer_x_ruti = file of ejercicios_x_rutina;
+    clie = file of clientes;
+    ruti_x_clie = file of rutinas_x_clientes;
 
 var
 	repet: cantidad_repeticiones;
 	pagoypeso: pago_en_pesos_y_peso_actual;
+
+    {registros}
+    g:gimnasio;
+    a:actividades;
+    dyh:dias_y_horarios;
+    exr:ejercicios_x_rutina;
+    c:clientes;
+    rxc:rutinas_x_clientes;
+
+    {archivos}
+    gim: gimna;
+    act: acti;
+    d_y_h: dia_y_hor;
+    eje_x_rut: ejer_x_ruti;
+    cli: clie;
+    rut_x_cli: ruti_x_clie;
+
+procedure ABM;
+var
+    op: Integer;
+
+    dir_gim: string;    {Variables con direcciones de archivos}
+    dir_act: string;
+    dir_d_y_h: string;
+    dir_eje_x_rut: string;
+    dir_cli: string;
+    dir_rut_x_cli: string;
+begin
+    dir_gim:= 'C:\tp-pascal\gimnasio.dat';
+    dir_act:= 'C:\tp-pascal\actividades.dat';
+    dir_d_y_h:= 'C:\tp-pascal\diasyhorarios.dat';
+    dir_eje_x_rut:= 'C:\tp-pascal\ejerciciosxrutina.dat';
+    dir_cli:= 'C:\tp-pascal\clientes.dat';
+    dir_rut_x_cli:= 'C:\tp-pascal\rutinasxclientes.dat';
+
+    clrscr;
+    writeln('Menu de ABM');
+    writeln('¿Que archivo quiere crear?');
+    writeln('1) Gimnasio');
+    writeln('2) Actividades');
+    writeln('3) Dias y Horarios');
+    writeln('4) Ejercicios por Rutinas');
+
+    repeat
+       readln(op);
+    until (op >=1) and (op <=4);
+
+    while op <> 5 do
+      begin
+          case op of
+             1: begin
+                   writeln('¿Quiere crear/abrir el archivo o quiere modificar algun campo?');
+                   writeln('1) Crear/Abrir Archivo');
+                   writeln('2) Modificar un campo');
+                   repeat
+                        readln(op);
+                   until (op >= 1) and (op <= 2);
+                   if op = 1 then
+                      begin
+                         assign(gim,dir_gim);
+                         {$I-}
+                         reset(gim);
+                         if ioresult = 2 then
+                            rewrite(act);
+                         {$I+}
+                         {writeln('Ingrese el nombre');
+                         read(g.nombre); }
+                         writeln('Ingrese la direccion');
+                         read(g.direccion);
+                         writeln('Ingrese el valor de la cuota');
+                         read(g.valor_cuota);
+                         writeln('Ingrese el valor del nutricionista');
+                         read(g.valor_nutricionista);
+                         writeln('Ingrese el valor del personal trainer');
+                         read(g.valor_personal_trainer);
+                      end
+                   else
+                      begin
+                         writeln('Ingrese un nuevo valor para el valor de la cuota');
+                         writeln('Ingrese un nuevo valor para el valor del nutricionista');
+                         writeln('Ingrese un nuevo valor para el valor del personal trainer');
+                      end;
+                end;
+
+             2: begin
+                   assign(act,dir_act);
+                   {$I-}
+                   reset(act);
+                   If ioresult =2 then
+                      rewrite(act);
+                   {$I+}
+
+                   writeln('Ingrese el codigo de la actividad');
+                   read(a.codigo_actividad);
+                   writeln('Ingrese la descripcion de la actividad');
+                   read(a.descripcion_actividad);
+                end;
+
+             3:  begin
+                    assign(d_y_h,dir_d_y_h);
+                    {$I-}
+                    reset(d_y_h);
+                    If ioresult =2 then
+                       rewrite(d_y_h);
+                    {$I+}
+
+                    writeln('Ingrese el dia');
+                    read(dyh.dia);
+                    writeln('Ingrese la hora');
+                    read(dyh.hora);
+                    writeln('Ingrese el codigo de la actividad');
+                    read(dyh.codigo_actividad);
+                 end;
+             4:  begin
+                    assign(rut_x_cli,dir_rut_x_cli);
+                    {$I-}
+                    reset(rut_x_cli);
+                    If ioresult =2 then
+                       rewrite(rut_x_cli);
+                    {$I+}
+
+                    writeln('Ingrese el codigo de ejercicio');
+                    read(exr.codigo_ejercicio);
+                    writeln('Ingrese la descripcion de la rutina');
+                    read(exr.descripcion_rutina);
+                end;
+          end;
+
+          repeat
+             readln(op);
+          until (op >=1) and (op <= 4);
+      end;
+end;
 
 procedure menu;
 var
@@ -74,7 +208,7 @@ begin
     while op <> 7 do
       begin
           case op of
-             1: writeln('Texto de prueba');
+             1: ABM;
              2: writeln('Texto de prueba');
              3: writeln('Texto de prueba');
              4: writeln('Texto de prueba');
